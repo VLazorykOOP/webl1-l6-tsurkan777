1ed0a0fdae968d5cbad5af72dcb2dad80d724dff
tsurkan777 <tsurkan.sofiia@chnu.edu.ua>
2025-10-23 11:41:16 +0300

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Цуркан Софія">
+    <title>Про автора</title>
+</head>
+<body style="background-color:#f8f5ef; font-family:Arial;">
+
+    <h4 dir="ltr" class="header" style="color:#4b2e83;">
+        Автор сайту: Цуркан Софія   
+    </h4>
+
+    <h5 align="center" style="color:#3b3b3b;" title="Мій девіз">
+        Читати — значить мислити!
+    </h5>
+
+    <h6 dir="ltr" class="subtitle" style="color:#666;">
+        Тема веб-проекту: Віртуальна бібліотека
+    </h6>
+
+    <p title="Інформація про автора" align="justify" id="about">
+        Мене звати <b class="highlight" lang="uk" dir="ltr">Цуркан Софія </b>. 
+        Я студентка спеціальності "Комп. Науки"  і розробляю вебсайт для проекту 
+        <tt id="project" title="Назва проєкту" lang="uk">«Бібліотека Онлайн»</tt>.<br id="info-br" class="text-break" style="margin-bottom:10px;">
+        Моя мета — створити сучасний зручний сайт для любителів книжок.
+    </p>
+
+    <hr noshade color="#4b2e83" size="2">
+
+    <p title="Мій девіз" align="center" id="motto">
+        <s style="color: gray;" dir="ltr">Я не зможу це зробити.</s><br>
+        <b class="motivation" lang="uk" dir="ltr">Я зможу все, якщо спробую!</b>
+    </p>
+
+</body>
+</html>
